10c99f48a51f337c4dec5801975c039762acad4e
orbtoo <47221292+orbtoo@users.noreply.github.com>
2019-07-23 23:04:16 +0430

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -11,12 +11,15 @@ else
     sudo add-apt-repository ppa:deadsnakes/ppa
     sudo apt-get update
     sudo apt-get install python3.6
+    
 fi
 
 
 clear
 echo "Python3.6 installed !!!"
 echo "Install Python Packages..."
+sudo python3.6 -m pip install --upgrade pip
+sudo python3.6 -m pip install --upgrade setuptools
 sudo python3.6 -m pip install -r requirements.txt
 sudo apt-get install redis-server
 
